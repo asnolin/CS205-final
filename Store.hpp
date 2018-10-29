32227b06bb43d1c34a36e4be2f79e13b96cf7204
asnolin <asnolin@uvm.edu>
2018-10-28 20:06:21 -0400

    first commit

---
+++ Store.hpp
@@ -0,0 +1,26 @@
+//includes
+//defs
+
+using namespace std;
+
+/*
+ * The Store Class
+ * contains:
+ * event queue
+ * lines for customers
+ * cashiers
+ */
+class Store{
+	private:
+		//fields
+			
+		//private functions
+		
+	public:
+		//constructor
+		Store();
+		//destructor
+		~Store();
+		//public functions
+		
+}//end store class
---
+++ customer.cpp
@@ -0,0 +1,16 @@
+//TODO
+using namespace std;
+
+enum status{SHOPPING, ARRIVE, READY, INLINE, DONE};
+
+class Customer{
+	private:
+	
+	public:
+	//constructor
+	//destructor
+	//wait
+	//intent to switch lines
+	//shop
+	//
+};//end shopper
