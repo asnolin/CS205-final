b180f49d0fb9e9239228056990658f95045ab3cd
asnolin <asnolin@uvm.edu>
2018-10-30 23:07:45 -0400

    customer is now a struct in Store

--- Store.cpp
+++ Store.cpp
@@ -10,11 +10,15 @@ Store::Store(){
 }//end store default constructor
 
 Store::~Store(){
-	lines->clear();
+	for(int i = 0; i < lines.size(); ++i){
+		lines[i].clear();
+	}
 }//end store default destructor
 
-Store::bool makeEvent(unsigned long int inTime, eventType inType, Customer* inCust){
-	return true;
+bool Store::makeEvent(unsigned long int inTime, eventType inType, Customer &inCust){
+	bool isMade = false;
+	
+	return isMade;
 }
 int main(int argc, char *argv[]){
 	return 0;
--- Store.hpp
+++ Store.hpp
@@ -1,41 +1,77 @@
 /*
  * Store.hpp
+ * The Store class contains:
+ * 	event queue
+ * 	event queue node struct
+ *	customer struct
+ *	checkout lines that hold customers
+ *	a vector to hold customers while they are shopping
+ * 	cashiers
+ *
+ * The Store class should communicate with GUI contorller 
+ * 
+ * TODO
+ * cashiers
+ * GUI
+ * n items or fewer lines
+ * customer movement
+ * add new cashiers
  */
 
 //lib includes
 #include<list>
 #include<queue>
+#include<vector>
 //class includes
-#include"Customer.hpp"
 //defs
 
 using namespace std;
 
-
-/*
- * The Store Class
- * contains:
- * event queue
- * 	the event queue should contain a time, pointer to customer, and a enum of event type
- * customers
- * lines for customers
- * cashiers
- */
 class Store{
 	private:
 		//fields
 		unsigned long int time;
-		enum eventType {CUSTOMER_ARRIVES, CUSTOMER_CHECKOUT_READY, CUSTOMER_CHECKOUT_FINISH, CUSTOMER_CHANGES_LINE, CUSTOMER_ABANDONS_LINE};
-		//structs 
+		//eventType is an enum for all types of events
+		enum eventType {CUSTOMER_ARRIVES, CUSTOMER_CHECKOUT_READY, CUSTOMER_CHECKOUT_FINISH, 
+				CUSTOMER_CHANGES_LINE, CUSTOMER_ABANDONS_LINE};
+
+
+		//customer struct holds data  associated with a customer
+		typedef struct{
+			//items is the number of items the customer has
+			//influences checkout time and shopping time
+			int items;
+
+			//timeIn is the time the customer first entered the store
+			unsigned long int timeIn;
+
+			//fidget is a probability 0-1
+			//determines how likley it is for a customer to switch lines or leave the store
+			double fidget;
+		}Customer;
+		
+
+		//event struct holds data needed for an event  
 		typedef struct{
 			unsigned long int exe_time;
 			eventType type;
 			Customer *cust;
-		}event;
-		//lines is an array of lists to hold customers
-		list<Customer> *lines;
-		priority_queue<event> eventQ;
+		}Event;
+
+
+		//lines is a vector of lists to hold customers
+		vector<list<Customer>>lines;
+		
+		//eventQ is the priority queue for Event structs sorted by time
+		priority_queue<Event> eventQ;
+
+		//shopping is the vector where customers will go when they enter the store but are not in line yet
+		vector<Customer> shopping;		
+		
 		//private functions
+
+		//makeEvent makes an event struct with the args passed in
+		// inserts event in sorted order in eventQ
 		bool makeEvent(unsigned long int inTime, eventType inType, Customer &inCust);
 	public:
 		//constructor
@@ -43,5 +79,6 @@ class Store{
 		//destructor
 		~Store();
 		//public functions
-		
+		//custIn creates a customer that arrives at time t
+		void customerIn(unsigned long int t);
 };//end store class
