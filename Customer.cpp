9f456522149fbc27cca9df0e0dd1983ff80dc7e7
William Slocum <wslocum@uvm.edu>
2018-11-07 15:36:57 -0500

    Created Customer Header File

---
+++ Customer.cpp
@@ -0,0 +1,21 @@
+class Customer{
+  public:
+    int id;
+    int items;
+    int imp;
+    int opp;
+    int inStore;
+    int inLine;
+
+    int getId();
+    int getItems();
+    int getImp();
+    int getOpp();
+    int getInStore();
+    int getInLine();
+
+    Customer();
+
+    ~Customer();
+
+}
