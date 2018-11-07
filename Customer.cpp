0f88e2a22a5907869b1616ffbd1cf7f3db4a0587
William Slocum <wslocum@uvm.edu>
2018-11-07 15:41:20 -0500

    Started Filling Out Customer.cpp

---
+++ Customer.cpp
@@ -0,0 +1,29 @@
+#include "Customer.hpp"
+
+int getId(){
+  return id;
+}
+
+int getItems(){
+  return items;
+}
+
+int getImp(){
+  return imp;
+}
+
+int getOpp(){
+  return opp;
+}
+
+int getInStore(){
+  return inStore;
+}
+
+int getInLine(){
+  return inLine;
+}
+
+Customer(){
+  
+}
