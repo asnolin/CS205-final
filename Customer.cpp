4eca02b3e7df78e05da6a7ed3e070ae2f3b83b55
William Slocum <wslocum@uvm.edu>
2018-11-07 16:00:20 -0500

    Code for Generating Random Number of Items

--- Customer.cpp
+++ Customer.cpp
@@ -24,7 +24,17 @@ int getInLine(){
   return inLine;
 }
 
+double genRandExp(double beta) {
+  double u, x;
+  u = drand48();
+  x = -beta * log(1.0 - u); // this is the natural log
+  return(x);
+}
+
 Customer(){
   id = idSeed;
   idSeed++;
+
+  items = genRandExp(meanNumItems)
+
 }
--- Customer.hpp
+++ Customer.hpp
@@ -9,12 +9,17 @@ class Customer{
 
     static int idSeed = 1;
 
+    // Mean Number of Items Shoppers will Have, Used in genRandExp
+    static int meanNumItems = 0;
+
     int getId();
     int getItems();
     int getImp();
     int getOpp();
     int getInStore();
     int getInLine();
+
+    double genRandExp(double beta);
 
     Customer();
 
