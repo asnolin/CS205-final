b2212f61bdc235830a27efac74ab2c2699f0fe18
Ylee <yli22@uvm.edu>
2018-11-09 15:32:42 -0500

    Checkout Line class

--- CheckoutLine.cpp
+++ CheckoutLine.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "CheckoutLine.h"
+#include <stdlib.h>
+CheckoutLine::CheckoutLine(int ID);
+
+int CheckoutLine::idSeed = 1;
+int CheckoutLine::CheckoutLine(){
+    ID = idSeed;
+    idSeed++;
+    WaitTime = 0;
+};
+
+int CheckoutLine::getID() const{
+    return ID;
+}
+int CheckoutLine::getNumCustomers() const{
+    return NumCustomers;
+};
+int CheckoutLine::getNumItems() const{
+    return NumItems
+};
+int CheckoutLine::getWaitTime() const{
+    return WaitTime;
+};
+void CheckoutLine::setID(int inputID){
+    ID = inputID;
+};
+void CheckoutLine::incNumCustomers(){
+    NumCustomers += 1;
+};
+void CheckoutLine::decNumCustomers(){
+    NumCustomers -= 1;
+};
+void CheckoutLine::updateNumItems(int n){
+    NumItems = n;
+};
+void CheckoutLine::updateWaitTime(int n){
+    WaitTime = n;
+};
--- CheckoutLine.h
+++ CheckoutLine.h
@@ -5,8 +5,42 @@
 #ifndef CS205_FINAL_CHECKOUTLINE_H
 #define CS205_FINAL_CHECKOUTLINE_H
 
+#include <string>
+
+using namespace std;
+
 
 class CheckoutLine {
+    private:
+        //fields
+        int ID;
+        int NumCustomers;
+        int NumItems;
+        int WaitTime;
+
+        static int idSeed;
+    public:
+        CheckoutLine();
+        CheckoutLine(int ID);
+        //getter
+        int getID() const;
+        int getNumCustomers() const;
+        int getNumItems() const;
+        int getWaitTime() const;
+
+        //setter
+        void setID(int ID);
+        //void setNumCustomers(int NumCustomers);
+        //void setNumItems(int NumItems);
+        //void setWaitTime(int WaitTime);
+
+        void incNumCustomers();
+        void decNumCustomers();
+        void updateNumItems(int n);
+        void updateWaitTime(int n);
+
+
+
 
 };
 
