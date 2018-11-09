2ec2aaf82afdf58290e3706253494257a0c53a01
Ylee <yli22@uvm.edu>
2018-11-09 15:31:16 -0500

    Checkout Line Class

---
+++ CheckoutLine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ylee on 11/9/2018.
+//
+
+#include "CheckoutLine.h"
---
+++ CheckoutLine.h
@@ -0,0 +1,14 @@
+//
+// Created by Ylee on 11/9/2018.
+//
+
+#ifndef CS205_FINAL_CHECKOUTLINE_H
+#define CS205_FINAL_CHECKOUTLINE_H
+
+
+class CheckoutLine {
+
+};
+
+
+#endif //CS205_FINAL_CHECKOUTLINE_H
