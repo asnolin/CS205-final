0f707843f3fca56464ed19794b28139d88d9b4cb
William Slocum <wslocum@uvm.edu>
2018-11-16 13:54:27 -0500

    More Testing

--- TestStore.cpp
+++ TestStore.cpp
@@ -5,6 +5,7 @@ int main()
   Store theStore;
 
   printf("\nTesting getTime and incTime\n");
+  printf("--------------------------------------------------\n");
   int i;
   i = theStore.getTime();
   printf("Should Print 0: %d\n",i);
@@ -21,6 +22,8 @@ int main()
   i = theStore.getTime();
   printf("Should Print 3: %d\n",i);
 
+  printf("\nTesting addCheckoutLine and chooseLine\n");
+  printf("--------------------------------------------------\n");
   CheckoutLine L1;
   CheckoutLine L2;
   CheckoutLine L3;
@@ -31,17 +34,19 @@ int main()
   theStore.addCheckoutLine(&L3);
   theStore.addCheckoutLine(&L4);
 
-  printf("Line 1: %d\n",theStore.Lines[0]->getID());
-  printf("Line 2: %d\n",theStore.Lines[1]->getID());
-  printf("Line 3: %d\n",theStore.Lines[2]->getID());
-  printf("Line 4: %d\n",theStore.Lines[3]->getID());
+  L1.updateNumItems(20);
+  L2.updateNumItems(10);
+  L3.updateNumItems(5);
+  L4.updateNumItems(15);
 
-  theStore.Lines[0]->updateNumItems(20);
-  theStore.Lines[1]->updateNumItems(10);
-  theStore.Lines[2]->updateNumItems(5);
-  theStore.Lines[3]->updateNumItems(15);
+  printf("Line 1 Should Have 20 Items: %d = %d\n",theStore.Lines[0]->getID(),theStore.Lines[0]->getNumItems());
+  printf("Line 2 Should Have 10 Items: %d = %d\n",theStore.Lines[1]->getID(),theStore.Lines[1]->getNumItems());
+  printf("Line 3 Should Have  5 Items: %d =  %d\n",theStore.Lines[2]->getID(),theStore.Lines[2]->getNumItems());
+  printf("Line 4 Should Have 15 Items: %d = %d\n",theStore.Lines[3]->getID(),theStore.Lines[3]->getNumItems());
 
   CheckoutLine *Shortest;
   Shortest = theStore.chooseLine();
-  printf("Should Be Line 3: %d\n", Shortest->getID());
+  printf("Shortest Line Should Be Line 3: %d\n", Shortest->getID());
+
+  printf("\n");
 }
