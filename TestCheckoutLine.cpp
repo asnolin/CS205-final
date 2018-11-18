b7276a5aa1b649a1c630dc9ebab2e8ba7ff9ae61
William Slocum <wslocum@uvm.edu>
2018-11-18 16:40:37 -0500

    Test CheckoutLine Changes

--- TestCheckoutLine.cpp
+++ TestCheckoutLine.cpp
@@ -12,31 +12,39 @@ int main() {
     Customer c1(1);
     Customer c2(2);
     Customer c3(2);
-    cout<<"Should be 1 and we get " <<c1.getId() <<endl;
-    cout<<"Should be 2 and we get " <<c2.getId() <<endl;
-    cout<<"Should be 3 and we get " <<c3.getId() <<endl;
-    cout <<"Should be 1 and we get " << Line1.getID() << endl;
-    cout <<"Should be 2 and we get " << Line2.getID() << endl;
-    cout << "Should be 3 and we get " <<Line3.getID() << endl;
-    cout << "Should be 4 and we get " <<Line4.getID() << endl;
+    Customer c4(4);
+    cout << "Testing getID()" << endl;
+    cout << "---------------" << endl;
+    cout << "CheckoutLine Id Should Be 1: " << Line1.getID() << endl;
+    cout << "CheckoutLine Id Should Be 2: " << Line2.getID() << endl;
+    cout << "CheckoutLine Id Should Be 3: " << Line3.getID() << endl;
+    cout << "CheckoutLine Id Should Be 4: " << Line4.getID() << endl << endl;
+
+    cout << "Testing incNumCustomers() and decNumCustomers()" << endl;
+    cout << "---------------" << endl;
+    cout << "//Line1.incNumCustomers(c1)" << endl;
+    cout << "//Line1.incNumCustomers(c2)" << endl;
+    cout << "//Line2.incNumCustomers(c3)" << endl;
+    cout << "//Line3.incNumCustomers(c4)" << endl;
+    cout << "//Line3.decNumCustomers(c4)" << endl;
+
     Line1.incNumCustomers(c1);
-    Line2.incNumCustomers(c2);
+    Line1.incNumCustomers(c2);
     Line2.incNumCustomers(c3);
-    Line2.decNumCustomers(c2);
-    Line3.updateNumItems(3);
-    Line4.updateWaitTime(20);
+    Line3.incNumCustomers(c4);
+    Line3.decNumCustomers(c4);
 
-    //should be 1
-    cout <<"Should be 1 and we get " <<Line1.getNumCustomers()<<endl;
-    //should be 1
-    cout << "Should be 1 and we get " <<Line2.getNumCustomers()<<endl;
-    //should be 3
-    cout <<"Should be 3 and we get " << Line3.getNumItems()<<endl;
-    //should be 20
-    cout <<"Should be 20 and we get " << Line4.getWaitTime()<<endl;
-    //
-    Line1.update();
-    cout<<Line1.getNumItems()<<endl;
-    cout<<Line1.getWaitTime()<<endl;
+    cout <<"CheckoutLine 1 Number Customers Should be 2: " << Line1.getNumCustomers() << endl;
+    cout <<"CheckoutLine 2 Number Customers Should be 1: " << Line2.getNumCustomers() << endl;
+    cout <<"CheckoutLine 3 Number Customers Should be 0: " << Line3.getNumCustomers() << endl << endl;
 
+
+    cout << "Testing incNumCustomers() and decNumCustomers()" << endl;
+    cout << "---------------" << endl;
+    cout << "//Line1.updateNumItems(20)" << endl;
+    cout << "//Line2.updateWaitTime(15)" << endl;
+    Line1.updateNumItems(20);
+    Line2.updateWaitTime(15);
+    cout << "CheckoutLine 1 NumItems Should be 20: " << Line1.getNumItems() << endl;
+    cout << "CheckoutLine 2 WaitTime Should be 15: " << Line2.getWaitTime() << endl << endl;
 };
