3673bc7098a50e73aee9601b5f511f0f4788337f
asnolin <asnolin@uvm.edu>
2018-11-23 10:40:14 -0500

    produces a window with no background

--- glutTest.cpp
+++ glutTest.cpp
@@ -1,14 +1,37 @@
+/*
+ * MUST COMPILE WITH -lGL AND -lglut FLAGS TO LINK PROPERLY WITH g++ 
+ *
+ */
+
 #include<GL/glut.h>
+//define height and width of window
+GLuint WIDTH = 600;
+GLuint HEIGHT = 600;
+
+
+//initGL is a function from my CS120 code. I do not know what it does
+void initGL(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0,WIDTH, HEIGHT, 0.0, 0.0f, 1.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
 
 int main(int argc, char **argv){
-	//init GLUT
+	//init GLUT window
 	glutInit(&argc, argv);
-	glutInitWindowPosition(-1, -1);
-	glutInitWindowSize(100,100);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutInitWindowPosition(0,0); //position of window's initiial top-left corner
+	//
+	
 	int mainWindow = glutCreateWindow("testing");
-	//register callbacks
-	//enter GLUT event processing cycle
+	initGL();
+	//glutDisplayFunc()
+	//glutKeyboardFunc()
 	glutMainLoop();
 	return 0;
 }//end main
